90a6d039b375572f560d1adaa42b489fbbac8460
lukas.moessler@gmail.com <lukas.moessler@gmail.com>
2023-04-09 23:16:31 +0200

    added rofi,hypr and fish#

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+
+alias exo='cd ~/Code/laravel/exotime_v2' 
+alias nv=nvim
+
+alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
