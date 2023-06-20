5807bf0d5e1b563ec87e202f67ffd81b7f037662
lukas.moessler@gmail.com <lukas.moessler@gmail.com>
2023-06-20 12:22:04 +0200

    emergency backup

--- fish/config.fish
+++ fish/config.fish
@@ -2,8 +2,32 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    # Greeting messages
+    set powered_msgs \
+        "candy!" \
+        "rubber bands" \
+        "a black hole" \
+        "logic" \
+        "electromagnetic cheese"
+    
+    # Randomly pick a message
+    set chosen_msg (random)"%"(count $powered_msgs)
+    set chosen_msg $powered_msgs[(math $chosen_msg"+1")]
+    
+    # Output it to the console
+    printf (set_color F90)"Welcome! This terminal session is powered by %s\n" $chosen_msg
+    echo (date +"%Y-%m-%d")
+end
+
+alias c=clear
+alias pac='sudo pacman'
 
 alias exo='cd ~/Code/laravel/exotime_v2' 
 alias nv=nvim
 
 alias sail='[ -f sail ] && sh sail || sh vendor/bin/sail'
+
+set TERM xterm-256color
+
+starship init fish | source
