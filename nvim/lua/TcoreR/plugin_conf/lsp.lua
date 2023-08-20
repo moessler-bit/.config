586047488de36cc9b96abac72418543dfef08506
lukas <lelukeonfly@gmail.com>
2023-08-20 16:03:58 +0200

    lsp config starting over now wip

--- nvim/lua/TcoreR/plugin_conf/lsp.lua
+++ nvim/lua/TcoreR/plugin_conf/lsp.lua
@@ -1,25 +1,45 @@
+-- local lsp = require('lsp-zero').preset({})
+
+-- lsp.on_attach(function(client, bufnr)
+--   lsp.default_keymaps({buffer = bufnr})
+-- end)
+
+-- lsp.setup()
+
+-- -- You need to setup `cmp` after lsp-zero
+-- local cmp = require('cmp')
+-- local cmp_action = require('lsp-zero').cmp_action()
+
+-- cmp.setup({
+--     mapping = {
+--         ['<C-p>'] = cmp.mapping.select_prev_item(),
+--         ['<C-n>'] = cmp.mapping.select_next_item(),
+--         ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+--         ['<C-d>'] = cmp.mapping.scroll_docs(4),
+--         ['<C-Space>'] = cmp.mapping.complete(),
+--         ['<C-e>'] = cmp.mapping.close(),
+--         ['<CR>'] = cmp.mapping.confirm({
+--             behavior = cmp.ConfirmBehavior.Replace,
+--             select = true,
+--         }),
+-- }})
 local lsp = require('lsp-zero').preset({})
+local cmp = require('cmp')
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
+
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+  vim.keymap.set('i', '<C-p>', cmp.mapping.select_prev_item())
+  vim.keymap.set('i', '<C-n>', cmp.mapping.select_next_item())
+  vim.keymap.set('i', '<C-u>', cmp.mapping.scroll_docs(-4))
+  vim.keymap.set('i', '<C-d>', cmp.mapping.scroll_docs(4))
+  vim.keymap.set('i', '<C-Space>', cmp.mapping.complete())
+  vim.keymap.set('i', '<C-e>', cmp.mapping.close())
+  vim.keymap.set('i', '<CR>', cmp.mapping.confirm({
+      -- behaviour = cmp.ConfirmBehavior.Replace,
+      select = true,
+  }))
 end)
 
 lsp.setup()
-
--- You need to setup `cmp` after lsp-zero
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-
-cmp.setup({
-    mapping = {
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-d>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        }),
-}})
--- nvim/lua/TcoreR/plugins.lua
+++ nvim/lua/TcoreR/plugins.lua
@@ -33,26 +33,20 @@ local plugins = {
     'tpope/vim-fugitive',
 
     {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'},
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
-    }
-  },
-
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    },
     -- code styling
     {
         'nvim-telescope/telescope.nvim',
